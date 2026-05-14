7d4d0b481346fd161ee16b4288e1f4dda458ffa0
Mareen Pigorsch <92370763+mareenp@users.noreply.github.com>
2026-05-14 15:29:39 +0200

    Add files via upload

---
+++ Results/MCT_Count_Powertable_anyall_4.R
@@ -0,0 +1,74 @@
+setwd("../2_results/Results_Simulation_Excel/")
+
+pacman::p_load(ggplot2, writexl, readxl, dplyr, tidyr, purrr, janitor, 
+               forcats, gridExtra, patchwork, cowplot, xtable)
+
+Paa4 <- read_excel("MCT_Count_power_anyall_4groups.xlsx")
+
+## change directory to the folder for tables
+setwd("../../2_results/Results_Tables/")
+
+############################# Power types ####################################
+Paa4_tab <- Paa4 %>%  
+  pivot_longer(cols = global:all,
+               names_to = "Powertype",
+               values_to = "Power") %>%
+  pivot_wider(names_from = "Method",
+              values_from = "Power") %>%
+  subset(select=c(Powertype,boot,hom,het,nb,q_poi,lambda,delta1,no,Dist,Contrast))
+
+
+##------------------------- Tables S1 - S4 --------------------------------##
+##### Dunnett
+### Table S1
+S1 <- Paa4_tab %>% filter(lambda==6, delta1==0, Contrast=="Dunnett") %>% 
+  subset(select=-c(Contrast, delta1)) %>% 
+  rename(Setting=no) %>%
+  mutate(across(2:6, ~ round(.x, 2)))
+## export Table S1 as Excel
+write_xlsx(S1, "TableS1.xlsx")
+## Latex Code for Table S1
+S1_latex <- xtable(S1,
+                   caption = "Power-types for contrast Dunnett with $\\tilde{\\lambda}_1=\\tilde{\\lambda}_2=\\lambda, \\tilde{\\lambda}_3=\\lambda + 1.5, \\tilde{\\lambda}_4=\\lambda + 2.5$ and $\\lambda=6$, resulting in one true and two false null hypotheses.")
+
+print(S1_latex, include.rownames = FALSE)
+
+### Table S2
+S2 <- Paa4_tab %>% filter(lambda==6, delta1==3, Contrast=="Dunnett") %>% 
+  subset(select=-c(Contrast, delta1)) %>% 
+  rename(Setting=no) %>%
+  mutate(across(2:6, ~ round(.x, 2)))
+## export Table S2 as Excel
+write_xlsx(S2, "TableS2.xlsx")
+## Latex Code for Table S2
+S2_latex <- xtable(S2,
+                   caption = "Power-types for contrast Dunnett, $\\tilde{\\lambda}_1=\\lambda, \\tilde{\\lambda}_2=\\lambda + 1.5, \\tilde{\\lambda}_3=\\lambda + 2.5, \\tilde{\\lambda}_4=\\lambda + 3$ and $\\lambda=6$, all null hypotheses are false.")
+
+print(S2_latex, include.rownames = FALSE)
+
+##### Tukey
+### Table S3
+S3 <- Paa4_tab %>% filter(lambda==6, delta1==0, Contrast=="Tukey") %>% 
+  subset(select=-c(Contrast, delta1)) %>% 
+  rename(Setting=no) %>%
+  mutate(across(2:6, ~ round(.x, 2)))
+## export Table S3 as Excel
+write_xlsx(S3, "TableS3.xlsx")
+## Latex Code for Table S3
+S3_latex <- xtable(S3,
+                   caption = "Power-types for contrast Tukey, $\\tilde{\\lambda}_1=\\tilde{\\lambda}_2=\\lambda, \\tilde{\\lambda}_3=\\lambda + 1.5, \\tilde{\\lambda}_4=\\lambda + 2.5$ and $\\lambda=6$, resulting in one true and five false null hypotheses.")
+
+print(S3_latex, include.rownames = FALSE)
+
+### Table S4
+S4 <- Paa4_tab %>% filter(lambda==6, delta1==3, Contrast=="Tukey") %>% 
+  subset(select=-c(Contrast, delta1)) %>% 
+  rename(Setting=no) %>%
+  mutate(across(2:6, ~ round(.x, 2)))
+## export Table S4 as Excel
+write_xlsx(S4, "TableS4.xlsx")
+## Latex Code for Table S4
+S4_latex <- xtable(S4,
+                   caption = "Power-types for contrast Tukey, $\\tilde{\\lambda}_1=\\lambda, \\tilde{\\lambda}_2=\\lambda + 1.5, \\tilde{\\lambda}_3=\\lambda + 2.5, \\tilde{\\lambda}_4=\\lambda + 3$ and $\\lambda=6$, all null hypotheses are false.")
+
+print(S4_latex, include.rownames = FALSE)
---
+++ Results/MCT_Count_Tables.R
@@ -0,0 +1,93 @@
+setwd("../2_results/Results_Tables/")
+
+devtools::install_github("thomas-fung/mpcmp")
+pacman::p_load(dplyr, stringr, xtable, mpcmp)
+
+###------------------------------- Table 1 ----------------------------------###
+table1 <- read.delim(text="Setting & Distrib. & Sample sizes & Dispersion & Dispersion parameter & Interpretation\
+1 & POI & n=n_1 + m & equidispersed & - & Balanced homosc. \
+2 & POI & n=n_2 + m & equidispersed & - & Unbalanced homosc. \
+3 & CMP & n=n_1 + m & overdispersed & nu=(0.5,0.5,0.5,0.5)' & Balanced homosc. \
+4 & CMP & n=n_1 + m & underdispersed & nu=(2,2,2,2)' & Balanced homosc. \
+5 & CMP & n=n_1 + m & overdispersed & nu=(0.2,0.2,0.5,0.5)' & Balanced heterosc. \
+6 & CMP & n=n_1 + m &  over- and underd. & nu=(0.2,0.5,2,2)' & Balanced heterosc. \
+7 & CMP & n=n_2 + m & overdispersed & nu=(0.5,0.5,0.2,0.2)' & Positive pairing \
+8 & CMP & n=n_2 + m & over- and underd. & nu=(2,2,0.5,0.2)' & Positive pairing \
+9 & CMP & n=n_2 + m & overdispersed & nu=(0.2,0.2,0.5,0.5)' & Negative pairing \
+10 & CMP & n=n_2 + m & over- and underd. & nu=(0.2,0.5,2,2)' & Negative pairing \
+11 & NB & n=n_1 + m & overdispersed & size=(3,3,3,3)' & Balanced homosc. \
+12 & NB & n=n_2 + m & overdispersed & size=(5,3,3,2)' & Positive pairing \
+13 & NB & n=n_2 + m & overdispersed & size=(0.75,2,2,3)' & Negative pairing \
+14 & NB & n=n_2 + m & overdispersed & size=(2,3,3,5)' & Negative pairing\ "
+,sep="&")
+
+### save Table 1 as Excel
+write_xlsx(table1, "Table1.xlsx")
+
+###------------------------------- Table 2 ----------------------------------###
+table2 <- read.delim(text="Characteristic_Sensitive_to & boot & hom & het & het_log & het_sqrt & poi & nb  & q_poi \
+# groups                                       & No  & No & No & No & No & No & No & No \
+Type of contrast                                  & No  & Yes & No  & Yes & Yes & Yes & Yes & Yes \
+Data Distribution                                 & No  & Yes & No  & Yes & Yes & Yes & Yes & Yes \
+Overdispersion                                    & No  & Yes & No  & Yes & Yes & Yes & No  & No \
+Underdispersion                                   & No  & Yes & No  & Yes & Yes & Yes & Yes & No \ 
+Small Sample Sizes combined with small lambda     & Yes & Yes & Yes & Yes & Yes & Yes & Yes & Yes \
+Small Sample Sizes combined with higher lambda    & Yes  & Yes & Yes & Yes & Yes & Yes & Yes & Yes", 
+sep="&")
+
+### save Table 2 as Excel
+write_xlsx(table2, "Table2.xlsx")
+
+###------------------------------- Table A1 ---------------------------------###
+tableA1 <- tibble(Distribution = c(paste("POI", 1:3), paste("CMP", 1:9),
+                                   paste("NB", 1:12)),
+                  Lambda = c(1,6,10,rep(1,3),rep(6,3),rep(10,3),
+                             rep(1,4),rep(6,4),rep(10,4)),
+                  Dispersion = c(NA,NA,NA,rep(c(0.2,0.5,2),3),rep(c(0.75,2,3,5),3)),
+                  Variance = rep(NA, 24),
+                  PropZero = rep(NA, 24),
+                  VarByMean = rep(NA, 24))
+
+tableA1 <- tableA1 %>% mutate(Variance=round(if_else(str_detect(Distribution, "POI"),Lambda,
+                                               if_else(str_detect(Distribution, "NB"),Lambda+Lambda^2/Dispersion,NA)),1))
+
+set.seed(123456)
+x1=lapply(1:3,function(arg){matrix(rpois(n=100000,lambda=tableA1$Lambda[1:3][arg]),ncol=1)})
+x2=lapply(1:9,function(arg){matrix(rcomp(n=100000,mu=tableA1$Lambda[4:12][arg],nu=tableA1$Dispersion[4:12][arg]),ncol=1)})
+x3=lapply(1:12,function(arg){matrix(rnbinom(n=100000,mu=tableA1$Lambda[13:24][arg],size=tableA1$Dispersion[13:24][arg]),ncol=1)})
+
+  
+tableA1$Variance[4:12] <- round(unlist(lapply(x2, function(mat) apply(mat, 2, var))),1)
+tableA1$PropZero[1:3] <- round(unlist(lapply(x1, function(arg) {sum(arg==0) / 100000})),2)
+tableA1$PropZero[4:12] <- round(unlist(lapply(x2, function(arg) {sum(arg==0) / 100000})),2)
+tableA1$PropZero[13:24] <- round(unlist(lapply(x3, function(arg) {sum(arg==0) / 100000})),2)
+
+tableA1 <- tableA1 %>% mutate(VarByMean=round(Variance/Lambda,1)) 
+
+### save Table A1 as Excel
+write_xlsx(tableA1, "TableA1.xlsx")
+
+### print for Latex
+A1_latex <- xtable(tableA1)
+print(A1_latex)
+
+###------------------------------- Table S5 ----------------------------------###
+tableS5 <- read.delim(text="Setting & Distrib. & Sample sizes & Dispersion & Dispersion parameter & Interpretation\
+1 & POI & n=n_1 + m & equidispersed & - & Balanced homosc. \
+2 & POI & n=n_2 + m & equidispersed & - & Unbalanced homosc. \
+3 & CMP & n=n_1 + m & overdispersed & nu=(0.5,0.5,0.5)' & Balanced homosc. \
+4 & CMP & n=n_1 + m & underdispersed & nu=(2,2,2)' & Balanced homosc. \
+5 & CMP & n=n_1 + m & overdispersed & nu=(0.2,0.5,0.5)' & Balanced heterosc. \
+6 & CMP & n=n_1 + m &  over- and underd. & nu=(0.2,0.5,2)' & Balanced heterosc. \
+7 & CMP & n=n_2 + m & overdispersed & nu=(0.5,0.5,0.2)' & Positive pairing \
+8 & CMP & n=n_2 + m & over- and underd. & nu=(2,0.5,0.2)' & Positive pairing \
+9 & CMP & n=n_2 + m & overdispersed & nu=(0.2,0.5,0.5)' & Negative pairing \
+10 & CMP & n=n_2 + m & over- and underd. & nu=(0.2,0.5,2)' & Negative pairing \
+11 & NB & n=n_1 + m & overdispersed & size=(3,3,3)' & Balanced homosc. \
+12 & NB & n=n_2 + m & overdispersed & size=(5,3,2)' & Positive pairing \
+13 & NB & n=n_2 + m & overdispersed & size=(0.75,2,3)' & Negative pairing \
+14 & NB & n=n_2 + m & overdispersed & size=(2,3,5)' & Negative pairing\ "
+                     ,sep="&")
+
+### save Table S5 as Excel
+write_xlsx(tableS5, "TableS5.xlsx")
